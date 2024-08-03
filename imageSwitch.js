68604db745ab82d099fa07ecbb11c7fee2ec7ed2
Gabriele Montefalcone <52413698+GabrieleMonte@users.noreply.github.com>
2024-08-02 21:03:15 -0500

    Create imageSwitch.js

---
+++ imageSwitch.js
@@ -0,0 +1,32 @@
+function updateValueDL(val){
+    document.getElementById('sliderValueDL').innerText = val;
+    callMyScriptDL(val);
+}
+
+function callMyScriptDL(value){
+    console.log("dl dampening slider value is: " + value);
+    var img = document.getElementById("Dlimage");
+    img.src = 'class_figures/Dl_Neff_damping_images/Dl_damping_Neff_v' + value + '.png';
+}
+
+function updateValuekTT(val){
+    document.getElementById('sliderValuekTT').innerText = val;
+    callMyScriptkTT(val);
+}
+
+function callMyScriptkTT(value){
+    console.log("kTT slider value is: " + value);
+    var img = document.getElementById("KTTImage");
+    img.src = 'class_figures/kTT_phase_shift_neff_lens_images/kTT_phase_shift_Neff_v' + value + '_lens.png';
+}
+
+function updateValuekEE(val){
+    document.getElementById('sliderValuekEE').innerText = val;
+    callMyScriptkEE(val);
+}
+
+function callMyScriptkEE(value){
+    console.log("kEE dampening slider value is: " + value);
+    var img = document.getElementById("kEEimage");
+    img.src = 'class_figures/kEE_phase_shift_neff_lens_images/kEE_phase_shift_Neff_v' + value + '_lens.png';
+}
