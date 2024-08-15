dbdb06d24ff49ec9a734b2c221a9e9738f4e8c18
igobyjac <136859155+igobyjac@users.noreply.github.com>
2024-08-14 20:50:39 -0500

    added n_s and a_s graphs for pps

--- imageSwitch.js
+++ imageSwitch.js
@@ -1,7 +1,7 @@
 // Neutrino effective values array (rounded to 2 decimal places)
 const N_eff_array = [0.1, 0.17, 0.24, 0.31, 0.37, 0.44, 0.51, 0.58, 0.65, 0.72, 0.79, 0.86, 0.92, 0.99, 1.06, 1.13, 1.2, 1.27, 1.34, 1.41, 1.47, 1.54, 1.61, 1.68, 1.75, 1.82, 1.89, 1.96, 2.02, 2.09, 2.16, 2.23, 2.3, 2.37, 2.44, 2.51, 2.57, 2.64, 2.71, 2.78, 2.85, 2.92, 2.99, 3.05, 3.12, 3.19, 3.26, 3.33, 3.4, 3.47, 3.53, 3.6, 3.67, 3.74, 3.81, 3.88, 3.95, 4.01, 4.08, 4.15, 4.22, 4.29, 4.36, 4.43, 4.5, 4.56, 4.63, 4.7, 4.77, 4.84, 4.91, 4.98, 5.04, 5.11, 5.18, 5.25, 5.32, 5.39, 5.46, 5.52, 5.59, 5.66, 5.73, 5.8, 5.87, 5.94, 6.0, 6.07, 6.14, 6.21, 6.28, 6.35, 6.42, 6.49, 6.55, 6.62, 6.69, 6.76, 6.83, 6.9];
-const ns_array = [0.8,  0.84, 0.87, 0.91, 0.94, 0.98, 1.01, 1.05, 1.09, 1.12, 1.16, 1.19, 1.23, 1.26, 1.3]
-const ln1010As_array = [1.95, 2.11, 2.27, 2.44, 2.6, 2.77, 2.93, 3.1, 3.26, 3.43, 3.59, 3.76, 3.92, 4.08, 4.25]
+const ns_array = [0.8,  0.84, 0.87, 0.91, 0.94, 0.98, 1.01, 1.05, 1.09, 1.12, 1.16, 1.19, 1.23, 1.26, 1.3, 1.33, 1.37, 1.4, 1.44, 1.47, 1.51, 1.54, 1.58, 1.61, 1.65, 1.68, 1.72, 1.75, 1.79, 1.82, 1.86]
+const ln1010As_array = [1.95, 2.11, 2.27, 2.44, 2.6, 2.77, 2.93, 3.1, 3.26, 3.43, 3.59, 3.76, 3.92, 4.08, 4.25, 4.41, 4.57, 4.73, 4.89, 5.05, 5.21, 5.37, 5.53, 5.69, 5.85, 6.01, 6.17, 6.33, 6.49, 6.65, 6.81, 6.97, 7.13, 7.29, 7.45, 7.61, 7.77, 7.93]
 // Function to update DL value
 function updateValueDL(val) {
     const N_eff_value = N_eff_array[val - 1];
@@ -57,6 +57,36 @@ function callMyScriptkEE(value) {
     var img = document.getElementById("kEEimage");
     img.src = 'class_figures/kEE_phase_shift_neff_lens_images/kEE_phase_shift_Neff_v' + value + '_lens.png';
 }
+
+// Function to update A_S value
+function updateValuea_s(val) {
+    const ln1010As_value = ln1010As_array[val-1];
+   document.getElementById('sliderValuea_s').innerText = ln1010As_value.toFixed(2);
+   callMyScripta_s(val)
+
+}
+
+// function to change a_s image
+function callMyScripta_s(value) {
+    console.log('a_s slider value is' + value);
+    var img = document.getElementById('imageAS');
+    img.src = 'class_figures/a_s_figures/Dl_a_s_' + value + '.png';
+}
+
+//function to update n_s value
+function updateValuen_s(val){
+    const ns_value = ns_array[val-1];
+    document.getElementById('sliderValuen_s').innerText = ns_value.toFixed(2);
+    callMyScriptn_s(val)
+}
+
+// function to update n_s image
+function callMyScriptn_s(value){
+    console.log('n_s slider value is: ' + value);
+    var img = document.getElementById('imageNS');
+    img.src = 'class_figures/n_s_figures/Dl_s_n' + value + '.png';
+}
+
 
 
 // Function to update As and ns values and change image
