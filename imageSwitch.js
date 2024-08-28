28a8aa595c0c3772b35d355494f62b0326f5472d
Gabriele Montefalcone <52413698+GabrieleMonte@users.noreply.github.com>
2024-08-27 22:52:15 -0500

    Update imageSwitch.js

--- imageSwitch.js
+++ imageSwitch.js
@@ -3,6 +3,29 @@ const N_eff_array = [0.1, 0.17, 0.24, 0.31, 0.37, 0.44, 0.51, 0.58, 0.65, 0.72, 
 const ns_array = [0.8,  0.84, 0.87, 0.91, 0.94, 0.98, 1.01, 1.05, 1.09, 1.12, 1.16, 1.19, 1.23, 1.26, 1.3, 1.33, 1.37, 1.4, 1.44, 1.47, 1.51, 1.54, 1.58, 1.61, 1.65, 1.68, 1.72, 1.75, 1.79, 1.82, 1.86]
 const ln1010As_array = [1.95, 2.11, 2.27, 2.44, 2.6, 2.77, 2.93, 3.1, 3.26, 3.43, 3.59, 3.76, 3.92, 4.08, 4.25, 4.41, 4.57, 4.73, 4.89, 5.05, 5.21, 5.37, 5.53, 5.69, 5.85, 6.01, 6.17, 6.33, 6.49, 6.65, 6.81, 6.97, 7.13, 7.29, 7.45, 7.61, 7.77, 7.93]
 const m_nus_array = [0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00]
+const Omega_L_pie_array = [0.409, 0.44 , 0.471, 0.502, 0.533, 0.564, 0.596, 0.627, 0.658, 0.689, 0.72 , 0.751, 0.782, 0.813, 0.844, 0.876, 0.907, 0.938, 0.969]
+const omega_m_pie_array = [0.099, 0.104, 0.109, 0.113, 0.118, 0.123, 0.128, 0.133, 0.138, 0.142, 0.147, 0.152, 0.157, 0.162, 0.167, 0.171, 0.176, 0.181, 0.186]
+const omega_b_pie_array = [0.0154, 0.0162, 0.017 , 0.0178, 0.0185, 0.0193, 0.0201, 0.0209, 0.0216, 0.0224, 0.0232, 0.024 , 0.0248, 0.0255, 0.0263, 0.0271, 0.0279, 0.0286, 0.0294]
+# Create all combinations of indices
+num_points = 15
+indices = np.arange(num_points)
+all_index_combinations = list(itertools.product(indices, repeat=3))
+
+# Create an array to store all combinations of parameter values
+num_combinations = num_points**3
+pie_pars_combinations = np.zeros((3, num_combinations))
+pie_ind_combinations = np.zeros((3, num_combinations), dtype=int)
+
+parameter_names = list(pie_pars_arrays.keys())
+
+# Fill the arrays with parameter values and indices
+for i, idx_comb in enumerate(all_index_combinations):
+    for j, param_name in enumerate(parameter_names):
+        pie_pars_combinations[j, i] = pie_pars_arrays[param_name][idx_comb[j]]
+        pie_ind_combinations[j, i] = idx_comb[j]
+pie_pars_combinations=np.transpose(pie_pars_combinations)
+print(np.shape(pie_pars_combinations))
+# Create all combinatio
 // Function to update DL value
 function updateValuemncdm(val) {
     const m_nus_value = m_nus_array[val - 1];
@@ -92,44 +115,50 @@ function callMyScriptn_s(value){
 
 // Function to update As and ns values and change image
 function updateCombinedValue() {
-    const As_index = document.getElementById('mySliderAs').value - 1;
-    const ns_index = document.getElementById('mySliderNs').value - 1;
+    const Omega_L_index = document.getElementById('mySliderOmegaL').value - 1;
+    const omega_m_index = document.getElementById('mySlideromegam').value - 1;
+    const omega_b_index = document.getElementById('mySlideromegab').value - 1;
+    const Omega_L_value = Omega_L_pie_array[Omega_L_index];
+    const omega_m_value = Omega_m_pie_array[omega_m_index];
+    const omega_b_value = Omega_b_pie_array[omega_b_index];
 
-    const As_value = ln1010As_array[As_index];
-    const ns_value = ns_array[ns_index];
+    document.getElementById('sliderValueOmegaL').innerText = Omega_L_value.toFixed(2);
+    document.getElementById('sliderValueomegam').innerText = omega_m_value.toFixed(2);
+    document.getElementById('sliderValueomegab').innerText = omega_b_value.toFixed(2);
 
-    document.getElementById('sliderValueAs').innerText = As_value.toFixed(2);
-    document.getElementById('sliderValueNs').innerText = ns_value.toFixed(2);
+    updateSliderBackground(Omega_L_index + 1, 'mySliderOmegaL');
+    updateSliderBackground(omega_m_index + 1, 'mySlideromegam');
+    updateSliderBackground(omega_b_index + 1, 'mySlideromegab');
 
-    updateSliderBackground(As_index + 1, 'mySliderAs');
-    updateSliderBackground(ns_index + 1, 'mySliderNs');
-
-    updateImage(As_index + 1, ns_index + 1);
+    updateImage(Omega_L_index + 1, omega_m_index + 1, omega_b_index + 1);
 }
 
-// Function to update image based on As and ns values
-function updateImage(As_index, ns_index) {
+// Function to update image based on Omega_L, omega_m, omega_b
+function updateImage(Omega_L_index, omega_m_index,omega_b_index) {
     const img = document.getElementById("Dlimage");
-    img.src = `class_figures/As_ns_figures/Dl_As_${As_index}_ns_${ns_index}.png`;
+    img.src = `class_figures/pie_chart_figures/Dl_pie_chart_OmegaL_${Omega_L_index}_omegam_${omega_m_index}_omegab_${omega_b_index}.png`;
 }
 
 // Function to initialize the sliders and their backgrounds
-function initialize_As_ns_Sliders() {
-    const initialAsValue = 8;
-    const initialNsValue = 6;
+function initialize_pie_chart_Sliders() {
+    const initialOmegaLValue = 10;
+    const initialomegamValue = 10;
+    const initialomegabValue = 10;
+    document.getElementById('mySliderOmegaL').value = initialOmegaLValue;
+    document.getElementById('mySlideromegam').value = initialomegamValue;
+    document.getElementById('mySlideromegab').value = initialomegabValue;
 
-    document.getElementById('mySliderAs').value = initialAsValue;
-    document.getElementById('mySliderNs').value = initialNsValue;
+    document.getElementById('sliderValueOmegaL').innerText = Omega_L_pie_array[initialOmegaLValue - 1].toFixed(2);
+    document.getElementById('sliderValueomegam').innerText = omega_m_pie_array[initialomegamValue - 1].toFixed(2);
+    document.getElementById('sliderValueomegam').innerText = omega_b_pie_array[initialomegabValue - 1].toFixed(2);
 
-    document.getElementById('sliderValueAs').innerText = ln1010As_array[initialAsValue - 1].toFixed(2);
-    document.getElementById('sliderValueNs').innerText = ns_array[initialNsValue - 1].toFixed(2);
-
-    updateSliderBackground(initialAsValue, 'mySliderAs');
-    updateSliderBackground(initialNsValue, 'mySliderNs');
+    updateSliderBackground(initialOmegaLValue, 'mySliderOmegaL');
+    updateSliderBackground(initialomegamValue, 'mySlideromegam');
+    updateSliderBackground(initialomegabValue, 'mySlideromegab');
 }
 
 // Call initializeSliders when the page loads
-window.onload = initialize_As_ns_Sliders;
+window.onload = initialize_pie_chart_Sliders;
 
 // Function to update slider background
 function updateSliderBackground(value, sliderId) {
